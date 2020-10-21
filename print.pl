533edb49efb98c25334c0908df20fd494266f6a9
BrenoAccioly <breno.accioly91@gmail.com>
2020-10-21 16:52:26 +0100

    Update print.pl

--- print.pl
+++ print.pl
@@ -1,6 +1,6 @@
 printCell(I, J) :-
 	board(I, J, V),
-	format(' ~d ', [V]).
+	((V >= 0) -> format('| ~d ', [V]); format('|~d ', [V])).
 
 printRow(I, J, Length) :- I < 5 , J > Length - 1.
 
@@ -12,12 +12,19 @@ printRow(I, J, Length) :-
 	printRow(I, NJ, Length).
 
 printVoidLeft(N) :-
-	N1 is 5 - N,
+	N1 is (5 - N)*2,
 	format('~*c', [N1, 35]).
 	
 printVoidRight(N) :-
-	N1 is 5 - N,
-	format('~*c ~n', [N1, 35]).
+	N1 is (5 - N)*2,
+	format('|~*c ~n', [N1, 35]).
+
+printBorderTop(N, Length) :-
+	N1 is N - 0.5,
+	Length1 is Length * 4,
+	printVoidLeft(N1),
+	format('~*s', [Length1, '/ \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\']),
+	printVoidRight(N1).
 
 printTopBoard(4).
 printTopBoard(N) :-
@@ -25,14 +32,16 @@ printTopBoard(N) :-
 	N1 is N + 1,
 	R is N mod 5,
 	Length is 5 + N,
+	printBorderTop(N, Length),
 	printVoidLeft(R),
 	printRow(N, 0, Length),
 	printVoidRight(R),
 	printTopBoard(N1).
 
 printMiddleBoard :-
+	printBorderTop(4, 9),
 	printVoidLeft(4),
-	printRow(4, 0, 8),
+	printRow(4, 0, 9),
 	printVoidRight(4).
 
 printBottomBoard(9).
@@ -42,6 +51,7 @@ printBottomBoard(N) :-
 	R is 8 mod N,
 	Length is 5 + R,
 	J is 9 - Length,
+	printBorderTop(R, Length),
 	printVoidLeft(R),
 	printRow(N, J, Length),
 	printVoidRight(R),
