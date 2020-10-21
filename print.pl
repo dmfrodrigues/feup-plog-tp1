1438029f6131b1e9b5748b5bb62e9b911f7637b9
BrenoAccioly <breno.accioly91@gmail.com>
2020-10-21 08:23:56 +0100

    Update print.pl

--- print.pl
+++ print.pl
@@ -55,20 +55,13 @@ board(5, 5 , 6).
 board(5, 6 , 7).
 board(5, 7 , 8).
 board(5, 8 , 9).
-board(5, 9 , 10).
 
-board(10, 0 , 5).
-board(10, 1 , 4).
-board(10, 2 , 3).
-board(10, 3 , 2).
-board(10, 4 , 1).
 
 board(9, 0 , 6).
 board(9, 1 , 5).
 board(9, 2 , 4).
 board(9, 3 , 3).
 board(9, 4 , 2).
-board(9, 5 , 1).
 
 board(8, 0 , 7).
 board(8, 1 , 6).
@@ -76,7 +69,6 @@ board(8, 2 , 5).
 board(8, 3 , 4).
 board(8, 4 , 3).
 board(8, 5 , 2).
-board(8, 6 , 1).
 
 board(7, 0 , 8).
 board(7, 1 , 7).
@@ -85,7 +77,6 @@ board(7, 3 , 5).
 board(7, 4 , 4).
 board(7, 5 , 3).
 board(7, 6 , 2).
-board(7, 7 , 1).
 
 board(6, 0 , 9).
 board(6, 1 , 8).
@@ -95,7 +86,6 @@ board(6, 4 , 5).
 board(6, 5 , 4).
 board(6, 6 , 3).
 board(6, 7 , 2).
-board(6, 8 , 1).
 
 printCell(I, J) :-
 	board(I, J, V),
@@ -117,6 +107,7 @@ printVoidRight(N) :-
 	N1 is 5 - N,
 	format('~*c ~n', [N1, 35]).
 
+printTopBoard(4).
 printTopBoard(N) :-
 	N < 4,
 	N1 is N + 1,
@@ -130,8 +121,9 @@ printTopBoard(N) :-
 printMiddleBoard :-
 	printRow(4, 8).
 
+printBottomBoard(10).
 printBottomBoard(N) :-
-	N < 9,
+	N < 10,
 	N1 is N + 1,
 	R is 9 mod N,
 	Length is 4 + R,
@@ -141,6 +133,6 @@ printBottomBoard(N) :-
 	printBottomBoard(N1).
 
 printBoard :-
-	printTopBoard(0);
-	printMiddleBoard;
-	printBottomBoard(5).
+	printTopBoard(0),
+	printMiddleBoard, nl,
+	printBottomBoard(6).
