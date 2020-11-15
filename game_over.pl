17882d04a676de19147f1daad16a68383f98f16a
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-15 14:33:44 +0000

    Removed enumerations in game_over, replaced by member

--- game_over.pl
+++ game_over.pl
@@ -29,90 +29,18 @@ game_over_(gamestate(Board, Turn), Winner) :-
 
 game_over_top(gamestate(Board, _), Winner) :- 
     ground(Board),
-    (
-        leadsTo(Board, Winner, [], 0-0, 8-4, _);
-        leadsTo(Board, Winner, [], 0-0, 8-5, _);
-        leadsTo(Board, Winner, [], 0-0, 8-6, _);
-        leadsTo(Board, Winner, [], 0-0, 8-7, _);
-        leadsTo(Board, Winner, [], 0-0, 8-8, _);
-        leadsTo(Board, Winner, [], 0-1, 8-4, _);
-        leadsTo(Board, Winner, [], 0-1, 8-5, _);
-        leadsTo(Board, Winner, [], 0-1, 8-6, _);
-        leadsTo(Board, Winner, [], 0-1, 8-7, _);
-        leadsTo(Board, Winner, [], 0-1, 8-8, _);
-        leadsTo(Board, Winner, [], 0-2, 8-4, _);
-        leadsTo(Board, Winner, [], 0-2, 8-5, _);
-        leadsTo(Board, Winner, [], 0-2, 8-6, _);
-        leadsTo(Board, Winner, [], 0-2, 8-7, _);
-        leadsTo(Board, Winner, [], 0-2, 8-8, _);
-        leadsTo(Board, Winner, [], 0-3, 8-4, _);
-        leadsTo(Board, Winner, [], 0-3, 8-5, _);
-        leadsTo(Board, Winner, [], 0-3, 8-6, _);
-        leadsTo(Board, Winner, [], 0-3, 8-7, _);
-        leadsTo(Board, Winner, [], 0-3, 8-8, _);
-        leadsTo(Board, Winner, [], 0-4, 8-4, _);
-        leadsTo(Board, Winner, [], 0-4, 8-5, _);
-        leadsTo(Board, Winner, [], 0-4, 8-6, _);
-        leadsTo(Board, Winner, [], 0-4, 8-7, _);
-        leadsTo(Board, Winner, [], 0-4, 8-8, _)
-    ).
+    member(U, [0-0,0-1,0-2,0-3,0-4]),
+    member(V, [8-4,8-5,8-6,8-7,8-8]),
+    leadsTo(Board, Winner, [], U, V, _).
 
 game_over_left(gamestate(Board, _), Winner) :- 
     ground(Board),
-    (
-        leadsTo(Board, Winner, [], 0-0, 4-8, _);
-        leadsTo(Board, Winner, [], 0-0, 5-8, _);
-        leadsTo(Board, Winner, [], 0-0, 6-8, _);
-        leadsTo(Board, Winner, [], 0-0, 7-8, _);
-        leadsTo(Board, Winner, [], 0-0, 8-8, _);
-        leadsTo(Board, Winner, [], 1-0, 4-8, _);
-        leadsTo(Board, Winner, [], 1-0, 5-8, _);
-        leadsTo(Board, Winner, [], 1-0, 6-8, _);
-        leadsTo(Board, Winner, [], 1-0, 7-8, _);
-        leadsTo(Board, Winner, [], 1-0, 8-8, _);
-        leadsTo(Board, Winner, [], 2-0, 4-8, _);
-        leadsTo(Board, Winner, [], 2-0, 5-8, _);
-        leadsTo(Board, Winner, [], 2-0, 6-8, _);
-        leadsTo(Board, Winner, [], 2-0, 7-8, _);
-        leadsTo(Board, Winner, [], 2-0, 8-8, _);
-        leadsTo(Board, Winner, [], 3-0, 4-8, _);
-        leadsTo(Board, Winner, [], 3-0, 5-8, _);
-        leadsTo(Board, Winner, [], 3-0, 6-8, _);
-        leadsTo(Board, Winner, [], 3-0, 7-8, _);
-        leadsTo(Board, Winner, [], 3-0, 8-8, _);
-        leadsTo(Board, Winner, [], 4-0, 4-8, _);
-        leadsTo(Board, Winner, [], 4-0, 5-8, _);
-        leadsTo(Board, Winner, [], 4-0, 6-8, _);
-        leadsTo(Board, Winner, [], 4-0, 7-8, _);
-        leadsTo(Board, Winner, [], 4-0, 8-8, _)
-    ).
+    member(U, [0-0,1-0,2-0,3-0,4-0]),
+    member(V, [4-8,5-8,6-8,7-8,8-8]),
+    leadsTo(Board, Winner, [], U, V, _).
 
 game_over_diagonal(gamestate(Board, _), Winner) :- 
     ground(Board),
-    (
-        leadsTo(Board, Winner, [], 4-0, 0-4, _);
-        leadsTo(Board, Winner, [], 4-0, 1-5, _);
-        leadsTo(Board, Winner, [], 4-0, 2-6, _);
-        leadsTo(Board, Winner, [], 4-0, 3-7, _);
-        leadsTo(Board, Winner, [], 4-0, 4-8, _);
-        leadsTo(Board, Winner, [], 5-1, 0-4, _);
-        leadsTo(Board, Winner, [], 5-1, 1-5, _);
-        leadsTo(Board, Winner, [], 5-1, 2-6, _);
-        leadsTo(Board, Winner, [], 5-1, 3-7, _);
-        leadsTo(Board, Winner, [], 5-1, 4-8, _);
-        leadsTo(Board, Winner, [], 6-2, 0-4, _);
-        leadsTo(Board, Winner, [], 6-2, 1-5, _);
-        leadsTo(Board, Winner, [], 6-2, 2-6, _);
-        leadsTo(Board, Winner, [], 6-2, 3-7, _);
-        leadsTo(Board, Winner, [], 6-2, 4-8, _);
-        leadsTo(Board, Winner, [], 7-3, 0-4, _);
-        leadsTo(Board, Winner, [], 7-3, 1-5, _);
-        leadsTo(Board, Winner, [], 7-3, 2-6, _);
-        leadsTo(Board, Winner, [], 7-3, 3-7, _);
-        leadsTo(Board, Winner, [], 7-3, 4-8, _);
-        leadsTo(Board, Winner, [], 8-4, 0-4, _);
-        leadsTo(Board, Winner, [], 8-4, 1-5, _);
-        leadsTo(Board, Winner, [], 8-4, 2-6, _);
-        leadsTo(Board, Winner, [], 8-4, 3-7, _);
-        leadsTo(Board, Winner, [], 8-4, 4-8, _)
-    ).
+    member(U, [4-0,5-1,6-2,7-3,8-4]),
+    member(V, [0-4,1-5,2-6,3-7,4-8]),
+    leadsTo(Board, Winner, [], U, V, _).
