8d6a1fab599e2f00fad8967e7dee2c404102b240
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-15 02:55:16 +0000

    Implemented game_over

--- game_over.pl
+++ game_over.pl
@@ -1,40 +1,118 @@
 :-
     use_module(library(lists)).
+:-
+    reconsult('board.pl').
+
+/**
+ * leadsTo(+Board, +Player, +U, +V, -Path)
+ * 
+ * True when U leads to V.
+ */
+leadsTo(Board, Player, Path, U, U, [U|Path]) :- isControlledByPlayer(Board, Player, U).
+leadsTo(Board, Player, Path, U, D,    Sol  ) :- isControlledByPlayer(Board, Player, U),
+    isAdj(Board, U, V),
+    isControlledByPlayer(Board, Player, V),
+    \+(member(V, Path)),
+    leadsTo(Board, Player, [U|Path], V, D, Sol).
 
 /**
  * game_over(+GameState, -Winner)​
  * 
  * Evaluates if game is over, and returns winner if it is over
  */
-game_over(Gamestate, Winner) :-
-    game_over_left(GameState, Winner);
-    game_over_top(GameState, Winner);
-    game_over_diag(GameState, Winner).
+game_over(gamestate(Board, Turn), 1) :- game_over_(gamestate(Board, Turn), 1).
+game_over(gamestate(Board, Turn), 2) :- game_over_(gamestate(Board, Turn), 2).
+game_over_(gamestate(Board, Turn), Winner) :-
+    game_over_top(gamestate(Board, Turn), Winner);
+    game_over_left(gamestate(Board, Turn), Winner);
+    game_over_diagonal(gamestate(Board, Turn), Winner).
 
-game_over_left(gamestate(Board, Turn), 1) :-
-    dfs(Board, 1, 0-0, Visited0),
-    dfs(Board, 1, 1-0, Visited1),
-    dfs(Board, 1, 2-0, Visited2),
-    dfs(Board, 1, 3-0, Visited3),
-    dfs(Board, 1, 4-0, Visited4),
-    append(Visited0   , Visited1, Visited01   ),
-    append(Visited01  , Visited2, Visited012  ),
-    append(Visited012 , Visited3, Visited0123 ),
-    append(Visited0123, Visited4, Visited01234),
-    remove_duplicates(Visited01234, Visited),
+game_over_top(gamestate(Board, _), Winner) :- 
+    ground(Board),
     (
-        member(4-8, Visited);
-        member(5-8, Visited);
-        member(6-8, Visited);
-        member(7-8, Visited);
-        member(8-8, Visited)
-    )
-    .
+        leadsTo(Board, Winner, [], 0-0, 8-4, _);
+        leadsTo(Board, Winner, [], 0-0, 8-5, _);
+        leadsTo(Board, Winner, [], 0-0, 8-6, _);
+        leadsTo(Board, Winner, [], 0-0, 8-7, _);
+        leadsTo(Board, Winner, [], 0-0, 8-8, _);
+        leadsTo(Board, Winner, [], 0-1, 8-4, _);
+        leadsTo(Board, Winner, [], 0-1, 8-5, _);
+        leadsTo(Board, Winner, [], 0-1, 8-6, _);
+        leadsTo(Board, Winner, [], 0-1, 8-7, _);
+        leadsTo(Board, Winner, [], 0-1, 8-8, _);
+        leadsTo(Board, Winner, [], 0-2, 8-4, _);
+        leadsTo(Board, Winner, [], 0-2, 8-5, _);
+        leadsTo(Board, Winner, [], 0-2, 8-6, _);
+        leadsTo(Board, Winner, [], 0-2, 8-7, _);
+        leadsTo(Board, Winner, [], 0-2, 8-8, _);
+        leadsTo(Board, Winner, [], 0-3, 8-4, _);
+        leadsTo(Board, Winner, [], 0-3, 8-5, _);
+        leadsTo(Board, Winner, [], 0-3, 8-6, _);
+        leadsTo(Board, Winner, [], 0-3, 8-7, _);
+        leadsTo(Board, Winner, [], 0-3, 8-8, _);
+        leadsTo(Board, Winner, [], 0-4, 8-4, _);
+        leadsTo(Board, Winner, [], 0-4, 8-5, _);
+        leadsTo(Board, Winner, [], 0-4, 8-6, _);
+        leadsTo(Board, Winner, [], 0-4, 8-7, _);
+        leadsTo(Board, Winner, [], 0-4, 8-8, _)
+    ).
 
-:-
-    reconsult('sample-states/final_state.pl'),
-    reconsult('print.pl'),
-    final_state(GameState),
-    display_game(GameState),
-    game_over(GameState, Winner),
-    format("Winner is ~d~n", [Winner]).
+game_over_left(gamestate(Board, _), Winner) :- 
+    ground(Board),
+    (
+        leadsTo(Board, Winner, [], 0-0, 4-8, _);
+        leadsTo(Board, Winner, [], 0-0, 5-8, _);
+        leadsTo(Board, Winner, [], 0-0, 6-8, _);
+        leadsTo(Board, Winner, [], 0-0, 7-8, _);
+        leadsTo(Board, Winner, [], 0-0, 8-8, _);
+        leadsTo(Board, Winner, [], 1-0, 4-8, _);
+        leadsTo(Board, Winner, [], 1-0, 5-8, _);
+        leadsTo(Board, Winner, [], 1-0, 6-8, _);
+        leadsTo(Board, Winner, [], 1-0, 7-8, _);
+        leadsTo(Board, Winner, [], 1-0, 8-8, _);
+        leadsTo(Board, Winner, [], 2-0, 4-8, _);
+        leadsTo(Board, Winner, [], 2-0, 5-8, _);
+        leadsTo(Board, Winner, [], 2-0, 6-8, _);
+        leadsTo(Board, Winner, [], 2-0, 7-8, _);
+        leadsTo(Board, Winner, [], 2-0, 8-8, _);
+        leadsTo(Board, Winner, [], 3-0, 4-8, _);
+        leadsTo(Board, Winner, [], 3-0, 5-8, _);
+        leadsTo(Board, Winner, [], 3-0, 6-8, _);
+        leadsTo(Board, Winner, [], 3-0, 7-8, _);
+        leadsTo(Board, Winner, [], 3-0, 8-8, _);
+        leadsTo(Board, Winner, [], 4-0, 4-8, _);
+        leadsTo(Board, Winner, [], 4-0, 5-8, _);
+        leadsTo(Board, Winner, [], 4-0, 6-8, _);
+        leadsTo(Board, Winner, [], 4-0, 7-8, _);
+        leadsTo(Board, Winner, [], 4-0, 8-8, _)
+    ).
+
+game_over_diagonal(gamestate(Board, _), Winner) :- 
+    ground(Board),
+    (
+        leadsTo(Board, Winner, [], 4-0, 0-4, _);
+        leadsTo(Board, Winner, [], 4-0, 1-5, _);
+        leadsTo(Board, Winner, [], 4-0, 2-6, _);
+        leadsTo(Board, Winner, [], 4-0, 3-7, _);
+        leadsTo(Board, Winner, [], 4-0, 4-8, _);
+        leadsTo(Board, Winner, [], 5-1, 0-4, _);
+        leadsTo(Board, Winner, [], 5-1, 1-5, _);
+        leadsTo(Board, Winner, [], 5-1, 2-6, _);
+        leadsTo(Board, Winner, [], 5-1, 3-7, _);
+        leadsTo(Board, Winner, [], 5-1, 4-8, _);
+        leadsTo(Board, Winner, [], 6-2, 0-4, _);
+        leadsTo(Board, Winner, [], 6-2, 1-5, _);
+        leadsTo(Board, Winner, [], 6-2, 2-6, _);
+        leadsTo(Board, Winner, [], 6-2, 3-7, _);
+        leadsTo(Board, Winner, [], 6-2, 4-8, _);
+        leadsTo(Board, Winner, [], 7-3, 0-4, _);
+        leadsTo(Board, Winner, [], 7-3, 1-5, _);
+        leadsTo(Board, Winner, [], 7-3, 2-6, _);
+        leadsTo(Board, Winner, [], 7-3, 3-7, _);
+        leadsTo(Board, Winner, [], 7-3, 4-8, _);
+        leadsTo(Board, Winner, [], 8-4, 0-4, _);
+        leadsTo(Board, Winner, [], 8-4, 1-5, _);
+        leadsTo(Board, Winner, [], 8-4, 2-6, _);
+        leadsTo(Board, Winner, [], 8-4, 3-7, _);
+        leadsTo(Board, Winner, [], 8-4, 4-8, _)
+    ).
