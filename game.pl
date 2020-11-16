7ec2939e52fd7f6970c2d8aadb5ef65020fdcad7
BrenoAccioly <breno.accioly91@gmail.com>
2020-11-16 22:01:26 +0000

    Added human vs human interface

--- game.pl
+++ game.pl
@@ -1,5 +1,8 @@
-:- reconsult('board_create.pl'),
-   reconsult('print.pl').
+:- consult('board_create.pl'),
+   consult('print.pl'),
+   consult('user_interface.pl'),
+   consult('game_over.pl'),
+   consult('moves.pl').
 
 /**
  * play()
@@ -7,13 +10,49 @@
  * Entry point of the game
  */
 play :-
+    initial_menu.
+
+/**
+ * play_game(+Mode)
+ * 
+ * Starts a match with the given Mode.
+ */
+play_game(h_h):-
     initial(GameState),
     !,
-    play_loop(GameState).
+    play_loop(GameState, h_h).
+
+play_game(c_c):-
+    initial(GameState),
+    !,
+    play_loop(GameState, c_c).
+
+/**
+ * play_game(+Mode, Level)
+ * 
+ * Starts a match with the given Mode and Level.
+ */
+play_game(h_c, Level):-
+    initial(GameState),
+    !,
+    play_loop(GameState, h_c).
+
+/**
+ *
+ */
+play_loop(gamestate(Board, Turn), h_h) :-
+    % Turn 1
+    display_game(gamestate(Board, Turn)),
+    turn_action(Turn, Board, NewBoard1),
+    end_turn(gamestate(NewBoard1, Turn), gamestate(NewBoard1, Turn1)),
     
-play_loop(GameState) :-
-    display_game(GameState),
-    end_turn(GameState, GameState1),
-    display_game(GameState1),
-    end_turn(GameState1, GameState2)
-    .
+    \+ game_over(gamestate(NewBoard1, Turn1), 1),
+
+    % Turn 2
+    display_game(gamestate(NewBoard1, Turn1)),
+    turn_action(Turn1, NewBoard1, NewBoard2),
+    end_turn(gamestate(NewBoard2, Turn1), gamestate(NewBoard2, Turn2)),
+
+    \+ game_over(gamestate(NewBoard2, Turn2), 2),
+
+    play_loop(gamestate(NewBoard2, Turn2), h_h).
--- user_interface.pl
+++ user_interface.pl
@@ -12,6 +12,12 @@ display_play_options :-
     format("3. Computer vs Computer~n", []),
     format("0. Back~n~n", []).
 
+display_choose_level :-
+    nl,
+    format("1. Level 1~n", []),
+    format("2. Level 2~n", []),
+    format("0. Back~n~n", []).
+
 display_instructions :-
     nl,
     format("Players take turns in doing two consecutive, mandatory actions:~n~n", []),
@@ -34,9 +40,49 @@ exec_initial_menu(1):-
     repeat,
     display_play_options,
     format("Option: ", []), read(Option),
-    exec_initial_menu(Option), !.
+    exec_play_options(Option), !.
 
 exec_initial_menu(2):-
     display_instructions,
     read(Option),
     initial_menu.
+
+exec_play_options(0) :-
+    initial_menu.
+
+% human vs human
+exec_play_options(1) :-
+    play_game(h_h).
+
+% human vs computer
+exec_play_options(2) :-
+    repeat,
+    display_choose_level,
+    format("Option: ", []), read(Option),
+    exec_play_options(Option), !.
+
+% computer vs computer
+exec_play_options(3) :-
+    play_game(c_c).
+
+exec_choose_level(0) :-
+    initial_menu.
+
+% level 1
+exec_choose_level(1) :-
+    play_game(h_c, 1).
+
+% level 2
+exec_choose_level(2) :-
+    play_game(h_c, 2).
+
+
+turn_action(Player, Board, NewBoard):-
+    repeat,
+    format("Movement~n", []),
+    format("Position: ", []), read(Pos),
+    format("Subtracks: ", []), read(Substracks),
+    format("Direction: ", []), read(Dir),
+    format("New Position: ", []), read(NewPos),
+    move(Board, playermove(Player, Pos, Substracks, Dir, NewPos), NewBoard),
+    !.
