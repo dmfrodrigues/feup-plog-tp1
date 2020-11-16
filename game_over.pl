e38aa93aeacafe995e06f1af3d06905fb2349642
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-16 22:06:08 +0000

    Implemented early stopping in DFS

--- game_over.pl
+++ game_over.pl
@@ -9,17 +9,21 @@
  * 
  * Finds nodes reachable from Stack.
  */
-dfs(Board, Player, [       ], Visited, Visited) :- !.
-dfs(Board, Player, [U|Stack], Visited, Ret    ) :- % Ignore if already in Visited
+dfs(Board, Player, [U|Stack], Visited, Dest   ) :- % Ignore if already in Visited
     member(U, Visited), !,
-    dfs(Board, Player, Stack, Visited, Ret).
-dfs(Board, Player, [U|Stack], Visited, Ret    ) :- % If U is under control, add adjacent
+    dfs(Board, Player, Stack, Visited, Dest   ).
+dfs(Board, Player, [U|Stack], Visited, Dest   ) :- % If U is under control, add adjacent
     isControlledByPlayer(Board, Player, U),
-    findall(V, isAdj(U, V), AddToStack),
-    append(AddToStack, Stack, NewStack),
-    dfs(Board, Player, NewStack, [U|Visited], Ret).
-dfs(Board, Player, [_|Stack], Visited, Ret   ) :- % U is not under control, ignore
-    dfs(Board, Player, Stack, Visited, Ret   ).
+    (
+        member(U, Dest);
+        (
+            findall(V, isAdj(U, V), AddToStack),
+            append(AddToStack, Stack, NewStack),
+            dfs(Board, Player, NewStack, [U|Visited], Dest)
+        )
+    ).
+dfs(Board, Player, [_|Stack], Visited, Dest   ) :- % U is not under control, ignore
+    dfs(Board, Player, Stack, Visited, Dest   ).
 
 /**
  * has_valid_moves(+Board, +Player)
@@ -45,15 +49,12 @@ game_over_(gamestate(Board, Turn), Winner) :-
 
 game_over_top(gamestate(Board, _), Winner) :- 
     ground(Board),
-    dfs(Board, Winner, [0-0,0-1,0-2,0-3,0-4], [], Res),
-    intersects([8-4,8-5,8-6,8-7,8-8], Res).
+    dfs(Board, Winner, [0-0,0-1,0-2,0-3,0-4], [], [8-4,8-5,8-6,8-7,8-8]).
 
 game_over_left(gamestate(Board, _), Winner) :- 
     ground(Board),
-    dfs(Board, Winner, [0-0,1-0,2-0,3-0,4-0], [], Res),
-    intersects([4-8,5-8,6-8,7-8,8-8], Res).
+    dfs(Board, Winner, [0-0,1-0,2-0,3-0,4-0], [], [4-8,5-8,6-8,7-8,8-8]).
 
 game_over_diagonal(gamestate(Board, _), Winner) :-
     ground(Board),
-    dfs(Board, Winner, [4-0,5-1,6-2,7-3,8-4], [], Res),
-    intersects([0-4,1-5,2-6,3-7,4-8], Res).
+    dfs(Board, Winner, [4-0,5-1,6-2,7-3,8-4], [], [0-4,1-5,2-6,3-7,4-8]).
