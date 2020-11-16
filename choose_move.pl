bb8baf3dfd47b927ab0c2a4b74b1ccb6636dfd9e
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-16 15:54:20 +0000

    Refactored choose_move

--- choose_move.pl
+++ choose_move.pl
@@ -17,7 +17,7 @@ choose_move(gamestate(Board, Turn), Turn, 1, Move) :-
  */
 best_move(gamestate(Board, Turn), [X|ListOfMoves], Move) :-
     move(Board, X, NewBoard),
-    (Turn =:= 1 -> Turn1 is 2 ; Turn1 is 1),
+    next_player(Turn, Turn1),
     value(gamestate(NewBoard, Turn1), Turn, V),
     best_move_(gamestate(Board, Turn), [X|ListOfMoves], Move, V).
 
@@ -29,19 +29,19 @@ best_move(gamestate(Board, Turn), [X|ListOfMoves], Move) :-
 best_move_(gamestate(Board, Turn), [X], X, V) :-
     !,
     move(Board, X, NewBoard),
-    (Turn =:= 1 -> Turn1 is 2 ; Turn1 is 1),
+    next_player(Turn, Turn1),
     value(gamestate(NewBoard, Turn1), Turn, V).
 best_move_(gamestate(Board, Turn), [X|ListOfMoves], X, V) :-
     % To the right
     best_move_(gamestate(Board, Turn), ListOfMoves, _, V1),
     % Current element
     move(Board, X, NewBoard),
-    (Turn =:= 1 -> Turn1 is 2 ; Turn1 is 1),
+    next_player(Turn, Turn1),
     value(gamestate(NewBoard, Turn1), Turn, V),
     % Evaluate
     (
         (Turn =:= 1, V >= V1);
-        (Turn =:= 2, V =< V1)    
+        (Turn =:= 2, V =< V1)
     ),
     !.
 best_move_(gamestate(Board, Turn), [_|ListOfMoves], X, V) :-
