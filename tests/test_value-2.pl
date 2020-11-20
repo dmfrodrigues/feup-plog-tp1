3db132fd3b0ea3e562b286432a7da96aa8767b50
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-20 20:24:14 +0000

    Improved test_value

--- tests/test_value-2.pl
+++ tests/test_value-2.pl
@@ -2,6 +2,7 @@
     reconsult('everything.pl'),
     intermediate_state(GameState),
     display_game(GameState),
-    \+(value(GameState, 1, 999999)),
+    value(GameState, 1, V),
+    V =:= 2,
     halt(0).
 :-  halt(1).
--- tests/test_value-4.pl
+++ tests/test_value-4.pl
@@ -4,6 +4,7 @@
     move(InitialBoard, playermove(1, 7-3, [6], 2, 8-8), NewBoard),
     GameState = gamestate(NewBoard, 2),
     display_game(GameState),
-    \+value(GameState, 1, 999999),
+    value(GameState, 1, V),
+    V =:= 7,
     halt(0).
 :-  halt(1).
