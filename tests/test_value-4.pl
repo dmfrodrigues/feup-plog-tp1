851da1da73e126130b285302b1ba4926ba98c477
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-20 20:58:52 +0000

    Made value faster

--- tests/test_value-4.pl
+++ tests/test_value-4.pl
@@ -1,6 +1,6 @@
 :-
     reconsult('everything.pl'),
-    initial_state(gamestate(InitialBoard,InitialTurn)),
+    initial_state(gamestate(InitialBoard,_)),
     move(InitialBoard, playermove(1, 7-3, [6], 2, 8-8), NewBoard),
     GameState = gamestate(NewBoard, 2),
     display_game(GameState),
