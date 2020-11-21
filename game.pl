2ea840fe56063929a1116793ac2dbac31fd6921d
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-21 20:55:17 +0000

    Encapsulated PC-user interaction

--- game.pl
+++ game.pl
@@ -38,6 +38,17 @@ play_game(h_c, Level):-
     !,
     play_loop(GameState, h_c, Level, 1).
 
+human_turn_action(gamestate(Board, Turn), NewGameState) :-
+    display_game(gamestate(Board, Turn)),
+    turn_action(Turn, Board, NewBoard),
+    end_turn(gamestate(NewBoard, Turn), NewGameState).
+computer_turn_action(gamestate(Board, Turn), NewGameState) :-
+    display_game(gamestate(Board, Turn)),
+    choose_move(gamestate(Board, Turn), Turn, 1, Move),
+    move(Board, Move, NewBoard),
+    display_computer_move(Move),
+    end_turn(gamestate(NewBoard, Turn), NewGameState).
+
 /**
  * play_loop(+GameState, +Mode, +Round)
  *
@@ -48,22 +59,18 @@ play_loop(gamestate(StartBoard, Turn), h_h, Round) :-
     display_round(Round),
     Round1 is Round+1,
     % Turn 1
-    turn_action(Turn, StartBoard, NewBoard1),
-    end_turn(gamestate(NewBoard1, Turn), gamestate(NewBoard1, Turn1)),
-
+    human_turn_action(gamestate(StartBoard, Turn), gamestate(NewBoard1, Turn1)),
     (
         (
             game_over(gamestate(NewBoard1, Turn), 1),
             display_game(gamestate(NewBoard1, Turn1)),
             display_game_over(Turn)
         );
-        (
-        % Turn 2
-        turn_action(Turn1, NewBoard1, NewBoard2),
-        end_turn(gamestate(NewBoard2, Turn1), gamestate(NewBoard2, Turn2)),
+        ( % Turn 2
+            human_turn_action(gamestate(NewBoard1, Turn1), gamestate(NewBoard2, Turn2)),
             (
-            game_over(gamestate(NewBoard2, Turn1), 2);
-            play_loop(gamestate(NewBoard2, Turn2), h_h, Round1)
+                game_over(gamestate(NewBoard2, Turn1), 2);
+                play_loop(gamestate(NewBoard2, Turn2), h_h, Round1)
             )
         )
     ).
@@ -72,27 +79,18 @@ play_loop(gamestate(StartBoard, Turn), c_c, Round) :-
     display_round(Round),
     Round1 is Round+1,
     % Turn 1
-    display_game(gamestate(StartBoard, Turn)),
-    choose_move(gamestate(StartBoard, Turn), Turn, 1, Move1),
-    move(StartBoard, Move1, NewBoard1),
-    display_computer_move(Move1),
-    end_turn(gamestate(NewBoard1, Turn), gamestate(NewBoard1, Turn1)),
-    
+    computer_turn_action(gamestate(StartBoard, Turn), gamestate(NewBoard1, Turn1)),
     (   
         (
             game_over(gamestate(NewBoard1, Turn), 1),
             display_game(gamestate(NewBoard1, Turn1)),
             display_game_over(Turn)
         );
-        (
-        % Turn 2
-        choose_move(gamestate(NewBoard1, Turn1), Turn1, 1, Move2),
-        move(NewBoard1, Move2, NewBoard2),
-        display_computer_move(Move2),
-        end_turn(gamestate(NewBoard2, Turn1), gamestate(NewBoard2, Turn2)),
+        ( % Turn 2
+            computer_turn_action(gamestate(NewBoard1, Turn1), gamestate(NewBoard2, Turn2)),
             (
-            game_over(gamestate(NewBoard2, Turn1), 2);
-            play_loop(gamestate(NewBoard2, Turn2), c_c, Round1)
+                game_over(gamestate(NewBoard2, Turn1), 2);
+                play_loop(gamestate(NewBoard2, Turn2), c_c, Round1)
             )
         )   
     ).
@@ -107,24 +105,18 @@ play_loop(gamestate(StartBoard, Turn), h_c, Level, Round) :-
     display_round(Round),
     Round1 is Round+1,
     % Turn 1
-    turn_action(Turn, StartBoard, NewBoard1),
-    end_turn(gamestate(NewBoard1, Turn), gamestate(NewBoard1, Turn1)),
-
+    human_turn_action(gamestate(StartBoard, Turn), gamestate(NewBoard1, Turn1)),
     (
         (
             game_over(gamestate(NewBoard1, Turn), 1),
             display_game(gamestate(NewBoard1, Turn1)),
             display_game_over(Turn)
         );
-        (
-        % Turn 2
-        choose_move(gamestate(NewBoard1, Turn1), Turn1, 1, Move),
-        move(NewBoard1, Move, NewBoard2),
-        display_computer_move(Move),
-        end_turn(gamestate(NewBoard2, Turn1), gamestate(NewBoard2, Turn2)),
+        ( % Turn 2
+            computer_turn_action(gamestate(NewBoard1, Turn1), gamestate(NewBoard2, Turn2)),
             (
-            game_over(gamestate(NewBoard2, Turn1), 2);
-            play_loop(gamestate(NewBoard2, Turn2), h_c, Level, Round1)
+                game_over(gamestate(NewBoard2, Turn1), 2);
+                play_loop(gamestate(NewBoard2, Turn2), h_c, Level, Round1)
             )
         )
     ).
