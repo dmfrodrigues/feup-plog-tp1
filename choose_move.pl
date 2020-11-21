8313bf4c2089dddb188e366aa8d008541abde300
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-21 15:16:31 +0000

    Added extra work on action1

--- choose_move.pl
+++ choose_move.pl
@@ -2,12 +2,44 @@
     reconsult('valid_moves.pl').
 
 /**
+ * valid_move_from_action1(+Board, +playermove_action1(Turn, Pos, Substacks, Dir), -playermove(Turn, Pos, Substacks, Dir, NewPos))
+ * 
+ * Given the first action of a move, find any complete move with that first action
+ * which is valid in a certain Board.
+ */
+valid_move_from_action1(
+  Board,
+  playermove_action1(Turn, Pos, Substacks, Dir),
+  playermove(Turn, Pos, Substacks, Dir, NewPos)
+) :-
+    move(Board, playermove(Turn, Pos, Substacks, Dir, NewPos), _).
+
+/**
+ * valid_moves_action1(+Board, +Turn, -ListOfMoves)
+ * 
+ * Find valid moves that vary in action 1, using for action 2 the first valid move
+ */
+valid_moves_action1(Board, Turn, ListOfMoves) :-
+    setof(
+        playermove_action1(Turn, Pos, Substacks, Dir),
+        Pos^Substacks^Dir^NewPos^NewBoard^move(Board, playermove(Turn, Pos, Substacks, Dir, NewPos), NewBoard),
+        ListOfAction1
+    ),
+    length(ListOfAction1, L),
+    list_create(Board, L, Boards),
+    maplist(valid_move_from_action1, Boards, ListOfAction1, ListOfMoves).
+
+/**
  * choose_move(+GameState, +Player, +Level, -Move)
  * 
  * Choose best move, using a certain level of difficulty
  */
 choose_move(gamestate(Board, Turn), Turn, Level, Move) :-
-    valid_moves(gamestate(Board, Turn), Turn, ListOfMoves),
+    choose_move_1(gamestate(Board, Turn), Turn, Level, Move).
+
+choose_move_1(gamestate(Board, Turn), Turn, Level, Move) :-
+    valid_moves_action1(Board, Turn, ListOfMoves),
+    best_N_moves(gamestate(Board, Turn), Level, ListOfMoves, 5, ListOfBestMoves),
     best_move(gamestate(Board, Turn), Level, ListOfMoves, Move).
 
 /**
