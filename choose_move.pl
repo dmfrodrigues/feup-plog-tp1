28ffe64aecf4826b12eb5a63139afb9b9b126496
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-22 18:25:59 +0000

    Joined choose_move_1 and 2

--- choose_move.pl
+++ choose_move.pl
@@ -1,5 +1,6 @@
 :-
-    reconsult('choose_move_1.pl').
+    reconsult('choose_move_1.pl'),
+    reconsult('choose_move_2.pl').
 
 
 /**
@@ -8,4 +9,13 @@
  * Choose best move, using a certain level of difficulty
  */
 choose_move(gamestate(Board, Turn), Turn, Level, N, Move) :-
-    choose_move_1(gamestate(Board, Turn), Turn, Level, N, Move, _).
+    choose_move_1(gamestate(Board, Turn), Turn, Level, N, Move1, Value1),
+    choose_move_2(gamestate(Board, Turn), Turn, Level, N, Move2, Value2),
+    (
+     (
+      (Turn =:= 1, Value1 >= Value2);
+      (Turn =:= 2, Value1 =< Value2)
+     )->
+        Move = Move1 ;
+        Move = Move2
+    ).
