21e5841606511531f303fa8a243e2e900a9739f6
BrenoAccioly <breno.accioly91@gmail.com>
2020-11-23 21:40:52 +0000

    Added board evaluation to readme and small changes

--- value.pl
+++ value.pl
@@ -10,8 +10,8 @@ value(gamestate(Board, _), 1, 999999) :-
 value(gamestate(Board, _), 2, -999999) :-
     game_over(gamestate(Board, _), 2), !.
 
-value(gamestate([L0, L1, L2, L3, L4, [P0|L5], [P1,P2|L6], [P0,P1,P2|L7], [P0,P1,P2,P3|L8]], P), _, Value) :-
-    GameState = gamestate([L0, L1, L2, L3, L4, [P0|L5], [P1,P2|L6], [P0,P1,P2|L7], [P0,P1,P2,P3|L8]], P),
+value(gamestate([L0, L1, L2, L3, L4, [P0|L5], [P1,P2|L6], [P0,P1,P2|L7], [P0,P1,P2,P3|L8]], _), _, Value) :-
+    GameState = gamestate([L0, L1, L2, L3, L4, [P0|L5], [P1,P2|L6], [P0,P1,P2|L7], [P0,P1,P2,P3|L8]], _),
     value_line(GameState, L0, 0-0, V0),
     value_line(GameState, L1, 1-0, V1),
     value_line(GameState, L2, 2-0, V2),
@@ -30,10 +30,10 @@ value_line(GameState, [N|Line], I-J, Value):-  % Valid position
     J1 is J+1,
     value_line(GameState, Line, I-J1, Value1),
     % Current value
-    position_value(I-J, F),
-    adjacent_to_another_cell(GameState, I-J, F2),
+    position_value(I-J, F1),
+    adjacent_to_another_stack(GameState, I-J, F2),
     (N =:= 0 ->
-    Value2 is 0; Value2 is F+(abs(N)**0.8 + 2)+F2),
+    Value2 is 0; Value2 is F1 + abs(N)**0.8 + 2 + abs(F2)),
     (N < 0 ->
     (Value is Value1 - Value2); (Value is Value1 + Value2)).
 
@@ -58,15 +58,16 @@ position_value(4-4, 4).
 
 position_value(_, 0).
 
-% add 2 points if cell is adjacent to another
-adjacent_to_another_cell(gamestate(Board, 1), I1-J1, 2):-
-    isControlledByPlayer(Board, 1, I1-J1),
+/**
+ * adjacent_to_another_stack(+GameState, +Pos, -Value)
+ * 
+ * Verifies if the stack in Pos is adjacent to another one of the
+ * same player.
+ */
+adjacent_to_another_stack(gamestate(Board, _), I1-J1, Value):-
+    isControlledByPlayer(Board, P, I1-J1),
     isAdj(I1-J1, I2-J2),
-    isControlledByPlayer(Board, 1, I2-J2),
+    isControlledByPlayer(Board, P, I2-J2),
+    (P = 1 -> Value is 2; Value is -2),
     !.
-adjacent_to_another_cell(gamestate(Board, 2), I1-J1, -2):-
-    isControlledByPlayer(Board, 2, I1-J1),
-    isAdj(I1-J1, I2-J2),
-    isControlledByPlayer(Board, 2, I2-J2),
-    !.
-adjacent_to_another_cell(_, _, 0).
+adjacent_to_another_stack(_, _, 0).
