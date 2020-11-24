78eacb001bf8f4aa9555c1710e1c070609abd782
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-24 23:18:09 +0000

    Forgotten cut

--- tests/test_value/2.pl
+++ tests/test_value/2.pl
@@ -2,7 +2,7 @@
     reconsult('../../value.pl'),
     reconsult('../../sample-states/intermediate_state.pl'),
     intermediate_state(GameState),
-    value(GameState, 1, V), !,
+    value(GameState, 1, V),
     V =:= 5.246014300255048,
     halt(0).
 :-  halt(1).
