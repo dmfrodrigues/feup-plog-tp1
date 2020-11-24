71bcd4d0cb2b975e7c48ea6b2640b74532f680e2
BrenoAccioly <breno.accioly91@gmail.com>
2020-11-24 23:10:36 +0000

    Fixes in test_value

--- tests/test_value/1.pl
+++ tests/test_value/1.pl
@@ -2,7 +2,9 @@
     reconsult('../../value.pl'),
     reconsult('../../sample-states/initial_state.pl'),
     initial_state(GameState),
-    value(GameState, 1, 0),
-    value(GameState, 2, 0),
+    value(GameState, 1, V1), 
+    value(GameState, 2, V2), !,
+    V1 =:= 0.0,
+    V2 =:= 0.0,
     halt(0).
 :-  halt(1).
--- tests/test_value/4.pl
+++ tests/test_value/4.pl
@@ -4,7 +4,7 @@
     initial_state(gamestate(InitialBoard,_)),
     move(InitialBoard, playermove(1, 7-3, [6], 2, 8-8), NewBoard),
     GameState = gamestate(NewBoard, 2),
-    value(GameState, 1, V),
-    V =:= 7,
+    value(GameState, 1, V), !,
+    V =:= 5.000000000000001,
     halt(0).
 :-  halt(1).
