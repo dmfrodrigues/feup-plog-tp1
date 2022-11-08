93048b7db8f128bc44d1f1886bfa42b7806db204
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2022-11-08 18:44:03 +0100

    Minor change

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-swipl -l server.pl -g "http_server([port($1)])" -g "thread_get_message(_)" -- PARALLEL_CMD=parallel
+(swipl -l server.pl -g "http_server([port($1)])" -g "thread_get_message(_)" -- PARALLEL_CMD=parallel) &
